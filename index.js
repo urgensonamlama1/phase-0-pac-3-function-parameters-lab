45e603a7761fa935f6a067fb105929ad3c7be3e7
urgensonamlama <urgensonamlama@gmail.com>
2021-08-27 23:13:52 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function introduction(name){
+
+    return `Hi, my name is ${name}.`;
+
+}
+
+function introductionWithLanguage(name,language){
+
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
+
+
+
+function introductionWithLanguageOptional(name,language ="User"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
+
+function introductionWithLanguageOptional(name,language ="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
